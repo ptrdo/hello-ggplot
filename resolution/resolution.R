5f2ea9c94179a33676008f08bf8b1b2f5021c834
ptrdo <ptrdo@users.noreply.github.com>
2023-03-11 11:45:21 -0800

    resolution

--- resolution/resolution.R
+++ resolution/resolution.R
@@ -6,20 +6,20 @@ ls()
 if (F) {
   install.packages('ggplot')
   install.packages('png')
-  # install.packages('ggfx')
 }
 
 library(ggplot2)
 library(png)
-# library(ggfx) # with_shadow
 
 theme_set(theme_gray())
 
-steps <- seq(-16,16,0.25)
+steps <- seq(-16,16,0.3)
 wave <- data.frame(Time=steps,Amplitude=cos(steps))
   
 p = ggplot(wave,aes(Time,Amplitude,colour='cosine')) +
   scale_color_manual(values=c('tomato'),name='wave') +
+  geom_point(color='white',size=3) +
+  geom_point(aes(labels='cosine'),color='red',size=1) +
   scale_x_continuous(
     expand=c(0,0), 
     limits=c(-16,16),
@@ -28,29 +28,23 @@ p = ggplot(wave,aes(Time,Amplitude,colour='cosine')) +
     expand=c(0,0), 
     limits=c(-4,4),
     breaks=c(-0.4,0,0.4)) +
-  xlab(label='Time') +
-  ylab(label='Amplitude') +
+  xlab(label='The x-axis should be exactly as wide as these words') +
+  ylab(label='The y-axis should be exactly this tall') +
   theme(
     text=element_text(size=unit(20,'pt')),
     plot.margin=margin(4,4,3,3,'lines'),
-    #plot.window=,
     panel.background=element_rect(fill='lightblue'),
     axis.ticks=element_blank(),
     axis.text=element_blank(),
-    legend.text=element_text(),
+    #legend.text=element_text(),
     legend.direction='horizontal',
-    legend.position=c(0.85,0.9),
+    legend.position=c(0.9,0.9),
     legend.background=element_rect(fill='white'),
-    legend.key=element_blank(),
+    #legend.key=element_blank(),
     legend.title=element_text(size=unit(15,'pt')),
     legend.margin=margin(c(0.5,1,0.5,1),unit="lines")
   ) +
-  ggtitle('Getting the Resolution Correct') +  
-  annotate('text', 0, 0, label='SIZE', color='white', alpha=0.7,
-    family='sans', fontface='bold', size=unit(48,'pt')) +
-  geom_point(color='white',size=4) +
-  geom_point(color='red',size=2)
-  # with_shadow(geom_point(size=2),sigma=5,x_offset=0,y_offset=30,colour='#8cc6d9')
+x
 
 
 
