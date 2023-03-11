99d46c50291c39ea41043a620bc95d3622098718
ptrdo <ptrdo@users.noreply.github.com>
2023-03-11 13:59:50 -0800

    resolution

--- resolution/resolution.R
+++ resolution/resolution.R
@@ -31,10 +31,11 @@ p = ggplot(wave,aes(Time,Amplitude,colour='key')) +
     limits=c(-4,4),
     breaks=c(-0.4,0,0.4)) +
   xlab(label='The x-axis should be exactly as wide as these words') +
-  ylab(label='The y-axis should be exactly this tall') +
+  ylab(label='The y-axis should be about this tall') +
+  annotate('text',-9.4,-1.6,label='Points should\nbarely touch',color='cyan',size=unit(4,'pt')) +
   theme(
     text=element_text(size=unit(20,'pt')),
-    plot.margin=margin(4,4,3,3,'lines'),
+    plot.margin=margin(3,3,3,3,'lines'),
     panel.background=element_rect(fill='darkslategrey'),
     axis.ticks=element_blank(),
     axis.text=element_blank(),
@@ -54,41 +55,61 @@ p = ggplot(wave,aes(Time,Amplitude,colour='key')) +
 
 p
 
-p$layers[[1]]$aes_params$size # annotation dim
 
+p$layers[[1]]$aes_params$size # annotation dim
 theme_get()$text$size
 
-p
 
-png('resolution/output/PLOTA.png', width=898, height=698) 
-p$layers[[1]]$aes_params$size <- unit(72,'pt')
-plot(p + (theme(text = element_text(size=unit(22,'pt')))))
+png('resolution/output/PLOTA.png', width=1200, height=900) 
+
+if (F) { 
+  # plot(p + (theme(axis.title.x=element_text(size=unit(44,'pt'))))) # does nothing
+  
+  p$layers[[1]]$aes_params$size <- unit(72,'pt') # corrects "SIZE" size
+  p$layers[[3]]$aes_params$size <- unit(8,'pt')
+  
+  p$theme$legend.title$size <- unit(36,'pt')
+  p$theme$legend.text$size <- unit(36,'pt')
+}
+
 plot(p)
 dev.off()
 
 
 ggsave(
-  filename = 'plot-2x4x3x200.png', 
+  filename = 'plot-4x3x3x200.png', 
   plot = p,
   device = png, 
   path = 'resolution/output',
-  scale = 2, # +/- dimension
+  units = c('in'),
   width = 4,
   height = 3, 
-  units = c('in'),
+  scale = 2, # +/- dimension
   dpi = 200 # +/- resolution
 )
 
 ggsave(
-  filename = 'plot-3x3x2x200.png', 
+  filename = 'plot-4x3x3x150.png', 
   plot = p,
   device = png, 
   path = 'resolution/output',
-  scale = 3.25, # +/- dimension
-  width = 3,
-  height = 2.25, 
   units = c('in'),
-  dpi = 200 # +/- resolution
+  width = 4,
+  height = 3, 
+  scale = 2, # +/- dimension
+  dpi = 150 # +/- resolution
+)
+
+ggsave(
+  filename = 'plot-4x3x3x300.png', 
+  plot = p,
+  device = png, 
+  path = 'resolution/output',
+  units = c('in'),
+  width = 4,
+  height = 3, 
+  scale = 2, # +/- dimension
+  dpi = 300 # +/- resolution
 )
 
 
